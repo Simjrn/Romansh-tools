fe4ba4363a01861bd778c7964e1153e870601bd0
Simjrn <simonjenni374@gmail.com>
2026-04-25 18:17:58 +0100

    Add Streamlit page navigation for home and exercises

---
+++ pages.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Define your pages
+home = st.Page("home.py", title="home", url_path="home", icon="🏠")
+excercises = st.Page("excercises.py", title="excercises", icon="🏋", url_path='activities')
+
+pg = st.navigation([home,excercises])
+pg.run()
