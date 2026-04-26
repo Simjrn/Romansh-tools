ed8964783d220b486095cd513cea22c2687c021e
Simjrn <simonjenni374@gmail.com>
2026-04-26 11:07:32 +0100

    Update contribute.py

--- contribute.py
+++ contribute.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-        {"": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
+        {"English": "My sentence in english", "Romansh": "My sentence translated into Romansh"}
     ]
 )
 edited_df = st.data_editor(df, num_rows="dynamic")
+st.write(edited_df)
