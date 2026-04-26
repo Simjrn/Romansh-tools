f4578c697c678a0fcc1ebfaa16b42f602c69ece7
Simjrn <simonjenni374@gmail.com>
2026-04-26 12:09:26 +0100

    Update pages.py

--- pages.py
+++ pages.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 # Define your pages
 home = st.Page("home.py", title="home", url_path="home", icon="🏠")
-excercises = st.Page("pages/excercises.py", title="excercises", icon="🏋", url_path='activities')
-contribute = st.Page("pages/contribute.py", title="contribute", icon="🎁", url_path='contribute')
+excercises = st.Page("excercises.py", title="excercises", icon="🏋", url_path='activities')
+contribute = st.Page("contribute.py", title="contribute", icon="🎁", url_path='contribute')
 edit = st.Page("add.py", title = "add", url_path='add')
 
 pg = st.navigation([home,excercises,contribute,edit], position="hidden")
