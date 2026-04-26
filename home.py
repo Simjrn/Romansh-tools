5122309cd508866c69caa35341e2cf33c758be65
Simjrn <simonjenni374@gmail.com>
2026-04-26 11:39:08 +0100

    Add link button for contributing sentences

--- home.py
+++ home.py
@@ -4,3 +4,4 @@ st.title("Welcome!")
 
 one, two = st.columns(2)
 one.link_button("Go to excercises", "https://romansh-tools.streamlit.app/activities", width="stretch")
+two.link_button("Contribute sentences", "https://romansh-tools.streamlit.app/contribute", width="stretch")
