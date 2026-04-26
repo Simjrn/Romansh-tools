94c112246b79f2a874071a7173088e93ba97ba28
Simjrn <simonjenni374@gmail.com>
2026-04-26 10:49:04 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.title("Welcome!")
 
-col1, col2 = st.columns(2)
-one = col1.container(width = "stretch", height = 234)
-two = col2.container(width = "stretch", height = 234)
-one.link_button("Go to excercises", "romansh-tools.streamlit.app/activities")
+one, two = st.columns(2)
+one.link_button("Go to excercises", "romansh-tools.streamlit.app/activities", )
