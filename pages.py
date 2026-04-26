380c3893e25594c4609a98cd61ac0b366486c4c3
Simjrn <simonjenni374@gmail.com>
2026-04-26 10:32:04 +0100

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,5 +4,5 @@ import streamlit as st
 home = st.Page("home.py", title="home", url_path="home", icon="🏠")
 excercises = st.Page("excercises.py", title="excercises", icon="🏋", url_path='activities')
 
-pg = st.navigation([home,excercises], posistion="hidden")
+pg = st.navigation([home,excercises], position="hidden")
 pg.run()
