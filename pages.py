74b976231bc7508bc605687bd21b8eaeaa79f60f
Simjrn <simonjenni374@gmail.com>
2026-04-26 11:32:06 +0100

    Add contribute and edit pages to navigation

--- pages.py
+++ pages.py
@@ -3,6 +3,8 @@ import streamlit as st
 # Define your pages
 home = st.Page("home.py", title="home", url_path="home", icon="🏠")
 excercises = st.Page("excercises.py", title="excercises", icon="🏋", url_path='activities')
+contribute = st.Page("contribute.py", title="contribute", icon="🎁", url_path='contribute')
+edit = st.Page("add.py", title = "add", url_path='add'
 
-pg = st.navigation([home,excercises], position="hidden")
+pg = st.navigation([home,excercises,contribute,edit], position="hidden")
 pg.run()
