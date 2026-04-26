db636c91cccf644c43e425b98ea2193f8fa81271
Simjrn <simonjenni374@gmail.com>
2026-04-26 11:29:40 +0100

    Update contribute.py

--- contribute.py
+++ contribute.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import pickle
+import os
 
 df = pd.DataFrame(
     [
@@ -8,3 +10,13 @@ df = pd.DataFrame(
 )
 edited_df = st.data_editor(df, num_rows="dynamic")
 st.write(edited_df)
+
+if st.button("submit"):
+    file_path = "sentences.pkl"
+
+    if os.path.exists(file_path):
+        with open(file_path, "ab") as f:
+            pickle.dump(edited_df, f)
+    else:
+        with open(file_path, "wb") as f:
+            pickle.dump(edited_df, f)
