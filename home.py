fad04d4cb50e54297c5243ae48b95354622fce4f
Simjrn <simonjenni374@gmail.com>
2026-04-26 09:42:07 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,9 +2,39 @@ import streamlit as st
 import pickle
 from streamlit_extras.resizable_columns import resizable_columns
 
+
+@st.dialog("Create account")
+def create_account():
+   left, right = st.columns((1, 3))
+
+    with left:
+        s = steps(
+            ["Name", "Colour", "Confirmation"],
+           icons=range(1, 4),
+            key="demo_vn",
+        )
+
+    with right:
+        with s[0]:
+            st.text_input("Your name", key="name_input")
+            if st.button("Next", key="vn_next_0"):
+                s.next()
+
+        with s[1]:
+            st.selectbox("Colour", ["Blue", "Red", "Green], key="colour_select")
+            with st.container(horizontal=True):
+                if st.button("Back", key="vn_back_1"):
+                   s.previous()
+                if st.button("Next", key="vn_next_1"):
+                    s.next()
+
+        with s[2]:
+            st.success("All done! You're all set.")
+            
+
+
 st.title("Welcome!")
 
-# 1. LOAD DATA: Try to open the file once. If it fails, create default data.
 try:
     with open("points.pkl", "rb") as f:
         data = pickle.load(f)
