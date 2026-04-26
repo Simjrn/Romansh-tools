aeebe3a0ddf9b06ec0b4a2a3f3de78bbab1b6e5d
Simjrn <simonjenni374@gmail.com>
2026-04-26 10:12:11 +0100

    Update excercises.py

--- excercises.py
+++ excercises.py
@@ -1,33 +1,6 @@
 import streamlit as st
 import random
 from streamlit_sortables import sort_items
-import pickle
-
-
-try:
-    with open("points.pkl", "rb") as f:
-        data = pickle.load(f)
-except (FileNotFoundError, EOFError):
-    data = {"points": 0}
-    with open("points.pkl", "wb") as f:
-        pickle.dump(data, f)
-
-
-
-
-
-from pathlib import Path
-
-def add_points(amount):
-    file_name = "points.pkl"
-    if Path(file_name).is_file():
-        with open(file_name, "rb") as f:
-            data = pickle.load(f)
-    data["points"] += amount
-    st.toast(f":green[+ {amount}!]")
-    with open(file_name, "wb") as f:
-        pickle.dump(data, f)
-
 
 page = st.sidebar.radio(
     "Choose which activity:",
@@ -67,7 +40,6 @@ if page == "Cloze deletions":
         if submit_button:
             if answer == random_str.replace('?', '').replace('.',''):
                 st.balloons()
-                add_points(15)
             else:
                 st.error(f"No, the correct answer was '{random_str}'")   
     get_answer()
@@ -101,7 +73,6 @@ elif page == "Verb conjugation exercises":
                         if button:
                             if answer == sentence.split()[1]:
                                 st.balloons()
-                                add_points(10)
                             else:
                                 st.error(f"Incorrect. The correct answer was '{sentence.split()[1]}'")
                     get_conj()
