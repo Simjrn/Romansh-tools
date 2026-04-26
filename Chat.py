3ee920e27a1656ec631ada21580a54b3beace2cc
Simjrn <simonjenni374@gmail.com>
2026-04-26 17:50:02 +0100

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import time
 
+def greet_chat():
+    st.chat_message("assistant").write("Allegra")
+    prompt = "Say hello back" 
+    while True:
+        input = st.chat_input(prompt)
+        if input == "Allegra":
+            st.chat_message("user").write("Allegra")
+            time.sleep(1)
+            st.chat_message("assistant").write("Co vai?")
+            prompt = "Reply with 'well, thank you'"
