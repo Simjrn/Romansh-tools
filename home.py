a045943693d3ca782099fe7e7a403f709d88cf38
Simjrn <simonjenni374@gmail.com>
2026-04-26 11:44:58 +0100

    Change link button type to primary

--- home.py
+++ home.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Welcome!")
 
 one, two = st.columns(2)
-one.link_button("Go to excercises", "https://romansh-tools.streamlit.app/activities", width="stretch")
+one.link_button("Go to excercises", "https://romansh-tools.streamlit.app/activities", width="stretch", type="primary")
 two.link_button("Contribute sentences", "https://romansh-tools.streamlit.app/contribute", width="stretch")
