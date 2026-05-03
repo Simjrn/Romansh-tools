4fd474b79cf7174e4a3307c9d7a80a20dc43a123
Simjrn <simonjenni374@gmail.com>
2026-05-03 17:14:13 +0100

    Update contribute.py

--- contribute.py
+++ contribute.py
@@ -16,9 +16,7 @@ if st.button("submit"):
     conn = st.connection("sentences_db", type="sql")
     
     with conn.session as s:
-        # Loop through each ROW of the edited dataframe
         for index, row in edited_df.iterrows():
-            # Use :variable syntax for security and to handle quotes/apostrophes
             query = text('INSERT INTO sentences (romansh, english) VALUES (:rom, :eng)')
             s.execute(query, params={"rom": row["Romansh"], "eng": row["English"]})
         
